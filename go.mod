c7546f1427c6d8a26c51c9bcbb266f9c2b01aee7
rorex33 <ilaris32@mail.ru>
2023-07-20 10:38:17 +0400

    code refactoring

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module test
 
 go 1.20
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
+
+require github.com/rorex33/dirsizecalc v1.0.3 // indirect
--- main.go
+++ main.go
@@ -1,45 +1,43 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"math"
 	"net/http"
 	"os"
-	"path/filepath"
 	"sort"
 	"strconv"
 	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/rorex33/dirsizecalc"
 )
 
-type nameSize struct {
-	name string
-	size float64
-}
-
 // Необходимо для сортировки
-type BySizeASC []nameSize
+type BySizeASC []dirsizecalc.NameSize
 
 func (a BySizeASC) Len() int           { return len(a) }
-func (a BySizeASC) Less(i, j int) bool { return a[i].size < a[j].size }
+func (a BySizeASC) Less(i, j int) bool { return a[i].Size < a[j].Size }
 func (a BySizeASC) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
-type BySizeDESC []nameSize
+type BySizeDESC []dirsizecalc.NameSize
 
 func (a BySizeDESC) Len() int           { return len(a) }
-func (a BySizeDESC) Less(i, j int) bool { return a[i].size > a[j].size }
+func (a BySizeDESC) Less(i, j int) bool { return a[i].Size > a[j].Size }
 func (a BySizeDESC) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
+//
+//
+
+// Приведение вещественного числа к виду с заданным количеством чисел после запятой.
 func roundFloat(val float64, precision uint) float64 {
 	ratio := math.Pow(10, float64(precision))
 	return math.Round(val*ratio) / ratio
 }
 
+// Проверка верности входных параметров.
 func validation(rootPath string, limit float64, sortType string) error {
 	if _, err := os.Stat(rootPath); os.IsNotExist(err) {
 		err := errors.New("validation fail: wrong root path")
@@ -60,94 +58,35 @@ func validation(rootPath string, limit float64, sortType string) error {
 
 }
 
-func dirSizeCalculation(path string, c chan<- float64) {
-	//Открываем канал sizes для передачи в него размеров вложенных дерикторий
-	sizes := make(chan int64)
+// Сортировка и вывод среза в терминал и выходной файл.
+func output(outPutArray []dirsizecalc.NameSize, rootPath string, limit float64, sortType string) error {
 
-	//Данная функция считает размер всех файлов, которые не являются директориями, и отправляет результат канал sizes
-	readSize := func(path string, file os.FileInfo, err error) error {
-		if err != nil || file == nil {
-			return nil
-		}
-		if !file.IsDir() {
-			sizes <- file.Size()
-		}
-		return nil
+	if sortType == "asc" {
+		sort.Sort(BySizeASC(outPutArray))
+	} else {
+		sort.Sort(BySizeDESC(outPutArray))
 	}
 
-	//Каждая горутина считывает размер открытой для неё папки и отправляет результат в канал sizes
-	//После завершения работы всег горутин канал закрывается
-	go func() {
-		filepath.Walk(path, readSize)
-		close(sizes)
-	}()
-
-	//Суммируем всё, что находится в нашем канале sizes
-	size := int64(0)
-	for s := range sizes {
-		size += s
-	}
-
-	//Возвращаем итоговый размер директории с учётом всех вложенных директорий
-	c <- float64(size)
-}
-
-func output(outPutArray []nameSize, limit float64, rootPath string) error {
 	//Создание выходного файла
-	file, err := os.Create("output.txt")
+	file, err := os.Create("/home/ivan/Desktop/githubProjects/directorySizeCounter/output.txt")
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-	x, _ := json.Marshal(outPutArray)
-	fmt.Println(string(x))
-	//Вывод данных в выходной файл и в терминал согласно условию задачи
+	// x, _ := json.Marshal(outPutArray)
+	// fmt.Println(string(x))
+	//Вывод данных в выходной файл (если размер больше лимита) и в терминал
 	for _, nameSizeValue := range outPutArray {
-		if nameSizeValue.size > limit {
-			file.WriteString(fmt.Sprintf("%s/%s	%s", rootPath, nameSizeValue.name, fmt.Sprintln(nameSizeValue.size)))
+		toWriteValue := fmt.Sprintf("%s/%s	%s", rootPath, nameSizeValue.Name, fmt.Sprintln(nameSizeValue.Size))
+		if nameSizeValue.Size > limit {
+			file.WriteString(toWriteValue)
 		}
-		fmt.Println(fmt.Sprintf("%s/%s	%s mb", rootPath, nameSizeValue.name, fmt.Sprint(roundFloat(nameSizeValue.size, 5))))
+		fmt.Println(toWriteValue)
 	}
 	return nil
 }
 
-func arrayCreation(rootPath string) ([]nameSize, error) {
-	//Срез структур с полями "имя" "размер" (для хранения имени и размера папок)
-	var nameSizeArray []nameSize
-
-	//Проходим по всем файлам указанной директории.
-	dirs, err := ioutil.ReadDir(rootPath)
-	if err != nil {
-		fmt.Println("Ошибка при чтении файлов ROOT директории:", err)
-		return nameSizeArray, err
-	}
-
-	//Если очередной файл явялется папкой, то вычисляем проводим необходимые действия
-	for _, dir := range dirs {
-		if !dir.IsDir() {
-			continue
-		}
-		//Вычисляем размер папки
-		c := make(chan float64)
-		defer close(c)
-		go dirSizeCalculation(fmt.Sprintf("%s/%s", rootPath, dir.Name()), c)
-		dirSize := <-c
-		dirSizeMb := dirSize / (1024 * 1024)
-		//Создаём переменную типа nameSize и добавления в срез nameSizeArray (размер папки переводится в мегабайты!)
-		nameSizeValue := nameSize{dir.Name(), dirSizeMb}
-		nameSizeArray = append(nameSizeArray, nameSizeValue)
-
-		//Обработка возможной ошибки при вовзращении в родительскую директорию
-		err = os.Chdir("..")
-		if err != nil {
-			fmt.Println("..")
-			return nameSizeArray, err
-		}
-	}
-
-	return nameSizeArray, nil
-}
-
+// Хендлер-функция для нашего запроса. Парсит параметры, запускает их валидацию, вычисления размеров директорий и вывод результата.
 func startCalculation(w http.ResponseWriter, r *http.Request) {
 	//Парсинг параметров
 	queries := r.URL.Query()
@@ -155,26 +94,19 @@ func startCalculation(w http.ResponseWriter, r *http.Request) {
 	limit, _ := strconv.ParseFloat(queries["limit"][0], 32)
 	sortType := strings.ToLower(queries["sort"][0])
 
-	//Проверка, что лимит и тип сортировки указаны верно
+	//Проверка верности указанных параметров
 	err := validation(ROOT, limit, sortType)
 	if err != nil {
 		fmt.Println(err)
 	} else {
 
-		//Создаём срез структур nameSize, в котором будут храниться имена и размеры папок указанной директории
-		nameSizeArray, err := arrayCreation(ROOT)
+		//Создаём срез, в котором будут храниться имена и размеры всех папок, находящихся в указанной директории
+		nameSizeArray, err := dirsizecalc.ArrayCreation(ROOT)
 		if err != nil {
 		}
 
-		//Сортировка полученного массива
-		if sortType == "asc" {
-			sort.Sort(BySizeASC(nameSizeArray))
-		} else {
-			sort.Sort(BySizeDESC(nameSizeArray))
-		}
-
-		//Вывод
-		err = output(nameSizeArray, limit, ROOT)
+		//Выводим результат
+		err = output(nameSizeArray, ROOT, limit, sortType)
 		if err != nil {
 			fmt.Println("Ошибка при создании выходного файла:", err)
 		}
